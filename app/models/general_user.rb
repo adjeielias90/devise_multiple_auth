977bdcae03cdc36bfdb8267e429c60f313af7c23
adjeielias90 <adjeielias90@gmail.com>
2019-06-19 16:30:36 +0000

    Add more providers

--- app/models/general_user.rb
+++ app/models/general_user.rb
@@ -2,5 +2,5 @@ class GeneralUser < ApplicationRecord
   has_secure_password
   validates :email, presence: true, uniqueness: true
   belongs_to :provider
-  has_many :ldap_users
+  has_many :ldap_users, dependent: :delete_all
 end
